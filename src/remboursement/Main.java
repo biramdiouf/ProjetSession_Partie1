38e21b7addd4059097d32dcc9c645f62b4982e84
biram <biram@172.29.75.213>
2015-02-11 12:50:43 -0500

    Renommage de la classe principale de test

---
+++ src/remboursement/Main.java
@@ -0,0 +1,52 @@
+package remboursement;
+
+/*
+ * Projet de session INF2015
+ ***
+ **** Biram DIOUF DIOB06127605
+ *
+ **** Anoushka J. GUIBOLA MALEKOU GUIA10618201
+ *
+ **** Latifa DAMOUH DAML11538206
+ *
+ **** Siham Kadi KADS06568801
+ *
+ */
+import net.sf.json.JSONObject;
+
+public class Main  {
+	
+
+	public static void main(String[] args) {
+
+		JSONObject fichierSortie;
+
+		if ((args.length != 2) || !args[0].endsWith(".json")
+				|| !args[1].endsWith(".json")) {
+			fichierSortie = new JSONObject();
+			fichierSortie.accumulate("message:", "Données invalides");
+		} else {
+			try {
+				String fichierEntree = FileReader.loadFileIntoString(args[0],
+						"utf-8");
+
+				if (Utilites.validerFichierJson(fichierEntree)) {
+					Reclamations reclam = new Reclamations();
+					fichierSortie = reclam.traiterReclamations(fichierEntree);
+				} else {
+					fichierSortie = new JSONObject();
+					fichierSortie.accumulate("message:", "Données invalides");
+				}
+
+			} catch (Exception ex) {
+				fichierSortie = new JSONObject();
+				fichierSortie.accumulate("message:", "Données invalides");
+			}
+		}
+		
+		Utilites.creerFichierSortie(fichierSortie, args[1]);
+	}
+
+
+
+}
